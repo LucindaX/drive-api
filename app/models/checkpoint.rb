95dd4439bd72943c508a12a79881125b22abb1a9
lucindax <ahmedosmanazrk@gmail.com>
2018-02-15 19:54:43 +0200

    adding virtual attr for coordinates

--- app/models/checkpoint.rb
+++ app/models/checkpoint.rb
@@ -1,4 +1,10 @@
 class Checkpoint < ApplicationRecord
+  attr_accessor :latlng
+  before_validation :convert_latlng
   belongs_to :trip
   validates_presence_of :lat, :lng
+
+  def convert_latlng
+    self.lat, self.lng = latlng if latlng
+  end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,13 @@
 class Trip < ApplicationRecord
+  attr_accessor :start_latlng, :end_latlng
+  before_validation :convert_latlng
   enum status: [:ongoing, :completed]
   has_many :checkpoints, dependent: :destroy
   validates_presence_of :start_lat, :start_lng
+
+  def convert_latlng
+    self.start_lat, self.start_lng = start_latlng if start_latlng
+    self.end_lat, self.end_lng = end_latlng if end_latlng
+  end
+
 end
